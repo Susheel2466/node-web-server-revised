3e3240e908ead6023e22d4e16cca5a2d8c21ca17
Apple <apple@Apples-MacBook-Air.local>
2019-09-26 00:48:29 +0530

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.port || 3000;
+
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', hbs);
@@ -59,6 +61,6 @@ app.get('/bad', (req, res) => {
         errorMessage: 'Unable to handle request'
     });
 });
-app.listen(3000, () => {
-    console.log('Server started at port 3000');
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
 });
